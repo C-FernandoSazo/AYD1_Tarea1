28946637ff246b0e1e2629a7dca70bc4db7b1ce2
César Fernando Sazo <cesarfernando.sazo@gmail.com>
2025-08-15 22:12:34 -0600

    fix(api): replace favorite song with favorite album

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get("/info", (req, res) => {
   res.json({
     nombre: "Cesar Fernando Sazo",
-    cancionFavorita: "Moviendo Caderas - Daddy Yanke"
+    albumFavorito: "Elevate - Big Time rush"
   });
 });
 
